5505b5f4ee7b317abb2963c9b09f100ee20302ee
Gabriel Saldanha <130391676+gbn0@users.noreply.github.com>
2023-06-02 13:56:20 -0300

    seila

--- src/components/DeMarvel.tsx
+++ src/components/DeMarvel.tsx
@@ -1,46 +1,65 @@
+import { useState, useEffect } from 'react'
 import Header from './Header';
 import './styles/DeMarvel.css';
 import demarvelImg from '../assets/demarvel.svg'
 import refreshImg from '../assets/refresh.svg'
+import trashcanImg from '../assets/trashcan.svg'
+import updateImg from '../assets/update.svg'
+import axios from 'axios';
 
 function DeMarvel() {
+    const [users, setUsers] = useState([]);
+    
+    async function getUsers() {
+        const users = await axios.get('http://localhost:5000/')
+        setUsers(users.data);
+    }
+
+    useEffect(() => {
+        getUsers();
+      }, []);
+
     return (
         <>
         <header>
             <div className="headerButtons">
                 <Header />
-                <button onClick={clickHandler} className="createBtn" >Create</button>
+                <button onClick={getUsers} className="createBtn" >Create</button>
             </div>
             <img src={demarvelImg} alt="" className="demarvel"/>
         </header>
             <main>
                 <div className="texts">
-                <p>NOME</p>
-                <p>FILME FAVORITO</p>
-                <p>HERÓI FAVORITO</p>
+                <p className="nameText">NOME</p>
+                <p className="favouriteMovie">FILME FAVORITO</p>
+                <p className="favouriteHero">HERÓI FAVORITO</p>
+                <button className="refreshBtn" onClick={getUsers} ><img src={refreshImg} alt="" className="refreshImg" /></button>
                 </div>
-                <button className="refreshBtn" ><img src={refreshImg} alt="" className="refreshImg" /></button>
+                
                 <div className="data">
-
+                {users.map((user, index) => (
+                <div className="child" key={index}>
+                    <textarea spellCheck={false} className="name" cols={1} rows={1} value={user.nick}></textarea>
+                    <textarea spellCheck={false} className="movie" cols={1} rows={1} value={user.gun}></textarea>
+                    <textarea spellCheck={false} className="hero" cols={1} rows={1} value={user.map}></textarea>
+                    <button className="updateBtn"><img src={updateImg} alt="" className="updateImg" /></button>
+                    <button className="deleteBtn"><img src={trashcanImg} alt="" className="trashcanImg" /></button>
+                </div>
+          ))}
                 </div>
 
-                <template>
+                {/* <template className='template' >
                     <div className='child'>
                         <textarea className='name' cols={1} rows={1}></textarea>
                         <textarea className='movie' cols={1} rows={1}></textarea>
                         <textarea className='hero' cols={1} rows={1}></textarea>
-                        <button><img src="" alt="" /></button>
-                        <button><img src="" alt="" /></button>
+                        <button><img src={updateImg} alt="" className="updateImg"/></button>
+                        <button><img src={trashcanImg} alt="" className="trashcanImg"/></button>
                     </div>
-                </template>
+                </template> */}
             </main>
         </>
     )
 }
-
-function clickHandler() {
-
-}
-
 
 export default DeMarvel;
